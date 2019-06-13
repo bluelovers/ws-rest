ba1fabf0644b874897b7c99c85082393be9531ee
bluelovers <codelovers@users.sourceforge.net>
2019-06-13 22:44:39 +0800

    export BluebirdOrigin

--- packages/bluebird-cancellation/index.ts
+++ packages/bluebird-cancellation/index.ts
@@ -1,7 +1,9 @@
 
 import IBluebird from 'bluebird'
 
-export const Bluebird = IBluebird.getNewLibraryCopy();
+export { IBluebird as BluebirdOrigin }
+
+export const Bluebird: typeof import('bluebird') = IBluebird.getNewLibraryCopy() as typeof import('bluebird');
 
 Bluebird.config({
 	cancellation: true
