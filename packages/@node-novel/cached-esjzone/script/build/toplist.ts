0c3c2d0b14307b2a820bca0e79aadf706f21bcad
bluelovers <codelovers@users.sourceforge.net>
2019-12-09 10:53:22 +0800

    cached-esjzone

---
+++ packages/@node-novel/cached-esjzone/script/build/toplist.ts
@@ -0,0 +1,134 @@
+/**
+ * Created by user on 2019/7/7.
+ */
+
+import fs from 'fs-extra';
+import path from 'upath2';
+import { IDmzjNovelInfoRecentUpdateRow, IDmzjNovelInfoWithChapters } from 'dmzj-api/lib/types';
+import { exportCache, importCache, processExitHook } from 'axios-cache-adapter-util';
+import { getAxiosCacheAdapter } from 'restful-decorator/lib/decorators/config/cache';
+import { IBaseCacheStore } from 'axios-cache-adapter-util';
+import Bluebird from 'bluebird';
+import { getApiClient, __root, console, consoleDebug } from '../util';
+import { ITSUnpackedPromiseLike } from 'ts-type';
+import { IESJzoneRecentUpdateCache, IESJzoneRecentUpdateRow } from 'esjzone-api/lib/types';
+
+export default (async () =>
+{
+	const { api, saveCache } = await getApiClient();
+
+	const file = path.join(__root, 'data', 'novel/recentUpdate.json');
+
+	let novelList = await (fs.readJSON(file)
+		.catch(e => null) as PromiseLike<IESJzoneRecentUpdateCache>)
+	;
+
+	let page = 0;
+	let pageTo = 0;
+	let maxPage = novelList && novelList.end || 0;
+
+	let API_METHOD: 'articleList' = 'articleList';
+	//API_METHOD = 'articleList';
+
+	let data = await api[API_METHOD](++page);
+	maxPage = data.end;
+
+	let pageFrom = data.page;
+
+	let { last_update_time } = data;
+	let lastPage = page;
+
+	let ids = [] as string[];
+
+	let list: IESJzoneRecentUpdateCache["data"] = data.data.slice();
+
+	list.forEach(row => {
+		ids.push(row.id);
+	});
+
+	let _do = true;
+
+	while (_do)
+	{
+		lastPage = page++;
+
+		consoleDebug.debug('page:', page);
+
+		let ret = await api[API_METHOD](page)
+			.tap(data => {
+
+				last_update_time = Math.max(last_update_time, data.last_update_time);
+
+				page = data.page;
+
+				data.data.forEach(row => {
+
+					if (!ids.includes(row.id))
+					{
+						ids.push(row.id);
+
+						list.push(row);
+					}
+
+				});
+
+			})
+			.catch(e => null)
+		;
+
+		if (!ret || lastPage == page || page == maxPage)
+		{
+			console.dir({
+				ret,
+				page,
+				lastPage,
+				maxPage,
+			});
+
+			_do = false;
+
+			break;
+		}
+
+		if ((page - pageFrom) >= 5)
+		{
+			//_do = false;
+		}
+
+	}
+
+	let dataNewList = (novelList && novelList.data || [])
+		.concat(list)
+		.reduce((list, row) => {
+
+			list[row.id] = row;
+
+			last_update_time = Math.max(last_update_time, row.last_update_time);
+
+			return list;
+		}, {} as Record<string, IESJzoneRecentUpdateRow>)
+	;
+
+	let listNew: IESJzoneRecentUpdateCache["data"] = Object
+		.values(dataNewList)
+		.sort((a, b) => {
+			//return b.last_update_time - a.last_update_time
+			// @ts-ignore
+			return b.id - a.id
+		})
+	;
+
+	let dataNew: IESJzoneRecentUpdateCache = {
+		from: pageFrom,
+		to: page,
+		end: maxPage,
+		last_update_time,
+		size: listNew.length,
+		data: listNew,
+	};
+
+	await fs.outputJSON(file, dataNew, {
+		spaces: 2,
+	});
+
+})();
---
+++ packages/@node-novel/cached-esjzone/script/util/files.ts
@@ -0,0 +1,33 @@
+import path from 'upath2';
+import { __root } from '../util';
+
+export const cacheFilePaths = {
+
+	recentUpdate: path.join(__root, 'data', 'novel/recentUpdate.json'),
+
+	task001: path.join(__root, 'test/temp', 'task001.json'),
+
+	copyrightRemove: path.join(__root, 'data/novel', 'copyright_remove.json'),
+
+	infoPack: path.join(__root, 'data', 'novel/info.pack.json'),
+
+	idAuthors: path.join(__root, 'data/novel', 'id_authors.json'),
+
+	idTitles: path.join(__root, 'data/novel', 'id_titles.json'),
+
+	idUpdate: path.join(__root, 'data/novel', 'id_update.json'),
+
+	ids: path.join(__root, 'data/novel', 'ids.json'),
+
+	titles: path.join(__root, 'data/novel', 'titles.json'),
+
+	authors: path.join(__root, 'data/novel', 'authors.json'),
+
+};
+
+export function cacheFileInfoPath(id: string | number)
+{
+	return path.join(__root, 'data', `novel/info/${id}.json`)
+}
+
+export default cacheFilePaths
