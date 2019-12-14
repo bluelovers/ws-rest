1a13ca90fa2bcf58cf43f5e624a385fdaf02cc92
bluelovers <codelovers@users.sourceforge.net>
2019-12-15 05:34:47 +0800

    .

--- packages/@node-novel/site-cache-util/lib/index.ts
+++ packages/@node-novel/site-cache-util/lib/index.ts
@@ -1,5 +1,7 @@
 import path from 'upath2';
 import { console, consoleDebug } from 'restful-decorator/lib/util/debug';
+import { ITSResolvable } from 'ts-type';
+import Bluebird from 'bluebird';
 
 export { path }
 export { console, consoleDebug }
@@ -10,4 +12,9 @@ consoleDebug.enabledColor = true;
 export function lazyImport<T = any>(name: string)
 {
 	return import(name).then(v => v.default as T)
+}
+
+export function lazyRun<T>(cb: (...argv: any) => ITSResolvable<T>)
+{
+	return Bluebird.resolve().then(cb)
 }
