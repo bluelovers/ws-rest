abb274e50402ca0118da19e616b14bcf47371795
bluelovers <codelovers@users.sourceforge.net>
2020-01-06 17:29:40 +0800

    FORCE_COLOR

--- packages/@node-novel/site-cache-util/lib/index.ts
+++ packages/@node-novel/site-cache-util/lib/index.ts
@@ -1,8 +1,14 @@
 import path from 'upath2';
-import { console, consoleDebug } from 'restful-decorator/lib/util/debug';
 import { ITSResolvable } from 'ts-type';
 import Bluebird from 'bluebird';
 import isCi from './ci';
+
+if (isCi())
+{
+	process.env.FORCE_COLOR = process.env.FORCE_COLOR || '1';
+}
+
+import { console, consoleDebug } from 'restful-decorator/lib/util/debug';
 
 export { path }
 export { console, consoleDebug }
@@ -13,6 +19,15 @@ consoleDebug.enabledColor = true;
 if (isCi())
 {
 	//consoleDebug.enabled = false;
+
+	let o = consoleDebug.inspectOptions || {};
+	o.colors = true;
+	consoleDebug.inspectOptions = o;
+
+	let o2 = consoleDebug.chalkOptions || {};
+	o2.enabled = true;
+	o2.level = o2.level || 1;
+	consoleDebug.chalkOptions = o2;
 }
 
 export function lazyImport<T = any>(name: string)
