b1ca5d191f8b344b459f1f3647b20b0426b91820
bluelovers <codelovers@users.sourceforge.net>
2020-01-07 05:16:54 +0800

    fix(data): fix moment timezone

--- script/ci/test/chk-moment.ts
+++ script/ci/test/chk-moment.ts
@@ -6,6 +6,12 @@ import { console } from '@node-novel/site-cache-util';
 let input = `2020-01-01`;
 let input_timestamp = 1577808000;
 
+console.dir(toMoment(input).unix());
+console.dir(toMoment(input).utcOffset());
+
+console.dir(moment(input).unix());
+console.dir(moment(input).utcOffset());
+
 console.dir(toMoment(input).utcOffset(480).unix());
 console.dir(toMoment(input).utcOffset(0).unix());
 
@@ -18,6 +24,14 @@ console.dir(unixMoment(1577808000).utcOffset(480).format());
 console.dir(toMoment().utcOffset());
 
 expect(toMoment(input).utcOffset(480).unix())
+	.to.deep.equal(input_timestamp)
+;
+
+expect(moment(input).utcOffset(480).unix())
+	.to.deep.equal(input_timestamp)
+;
+
+expect(toMoment(input).unix())
 	.to.deep.equal(input_timestamp)
 ;
 
