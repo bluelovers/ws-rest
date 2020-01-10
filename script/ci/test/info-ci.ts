7ccb58d603caf38238894885f4ace5f402ccc9a8
bluelovers <codelovers@users.sourceforge.net>
2020-01-10 16:22:08 +0800

    check(actions): show env ci

--- script/ci/test/info-ci.ts
+++ script/ci/test/info-ci.ts
@@ -4,12 +4,35 @@
 import { lazyRun, path, console } from '@node-novel/site-cache-util/lib/index';
 import envCi from 'env-ci';
 import { __rootWs } from '../../project-root';
+import crossCi from 'cross-ci';
 
 export default lazyRun(async () => {
 
+
+	console.cyan.log('env-ci');
 	console.dir(envCi({
 		cwd: __rootWs,
 	}));
+
+	console.cyan.log('cross-ci');
+
+	let vls = {
+		...crossCi.vars,
+	};
+
+	Object.keys(vls)
+		.forEach((k) => {
+
+			if (/token/i.test(k))
+			{
+				// @ts-ignore
+				vls[k] = `***(${k in vls}, ${(vls[k] != null) && vls[k].length})***`;
+			}
+
+		})
+	;
+
+	console.dir(vls);
 
 }, {
 	pkgLabel: __filename,
