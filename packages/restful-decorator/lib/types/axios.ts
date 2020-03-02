a99413cc5092d2641612fca073ab39dd951a8c6e
bluelovers <codelovers@users.sourceforge.net>
2020-03-03 05:05:17 +0800

    update deps

--- packages/restful-decorator/lib/types/axios.ts
+++ packages/restful-decorator/lib/types/axios.ts
@@ -1,9 +1,9 @@
 import _axios from 'axios';
 import { extendAxios, IAxiosCacheAdapterOptions, mixinCacheConfig, IBluebird, AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse, IAxiosAdapterWarpper, IAxiosDefaultsHeaders, IAxiosResponseClientRequest, IAxiosRetryConfig, IBluebirdAxiosResponse, IHttpheaders, IHttpheadersValues, IUnpackAxiosResponse, IUnpackedPromiseLikeReturnType, setupCacheConfig, wrapAdapter, AxiosAdapter, AxiosPromise, AxiosStatic } from '@bluelovers/axios-extend';
 
+export const axios = extendAxios(_axios).axios;
+export default axios;
+
 export * from '@bluelovers/axios-extend'
 
 export { extendAxios, IAxiosCacheAdapterOptions, mixinCacheConfig, IBluebird, AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse, IAxiosAdapterWarpper, IAxiosDefaultsHeaders, IAxiosResponseClientRequest, IAxiosRetryConfig, IBluebirdAxiosResponse, IHttpheaders, IHttpheadersValues, IUnpackAxiosResponse, IUnpackedPromiseLikeReturnType, setupCacheConfig, wrapAdapter, AxiosAdapter, AxiosPromise, AxiosStatic };
-
-export const axios = extendAxios(_axios).axios;
-export default axios;
