58cea0f03f7499db5b7d88fd5a693258ff186a1b
bluelovers <codelovers@users.sourceforge.net>
2020-03-06 09:51:28 +0800

    .

--- packages/restful-decorator/lib/decorators/config/util.d.ts
+++ packages/restful-decorator/lib/decorators/config/util.d.ts
@@ -4,6 +4,6 @@
 import 'reflect-metadata';
 import { IPropertyKey } from 'reflect-metadata-util';
 export declare const SymConfig: unique symbol;
-export declare function getConfig(target: any, propertyName?: IPropertyKey): any;
-export declare function setConfig(value: object, target: any, propertyName?: IPropertyKey): void;
+export declare function getConfig<T extends any = any>(target: any, propertyName?: IPropertyKey): T;
+export declare function setConfig<T extends any>(value: T, target: any, propertyName?: IPropertyKey): void;
 export declare function hasConfig(target: any, propertyName?: IPropertyKey): boolean;
