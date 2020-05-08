633cc5470ab78b00fbbda43834ea39952df6d85e
bluelovers <codelovers@users.sourceforge.net>
2020-05-09 01:15:07 +0800

    feat: excludeFromCache?: boolean,

--- packages/@bluelovers/axios-extend/lib/types.ts
+++ packages/@bluelovers/axios-extend/lib/types.ts
@@ -62,6 +62,14 @@ export interface IAxiosResponseClientRequest extends Record<IPropertyKey, any>
 	fromCache?: boolean,
 }
 
+declare module 'axios-cache-adapter'
+{
+	interface IAxiosCacheAdapterOptions
+	{
+		excludeFromCache?: boolean,
+	}
+}
+
 declare module 'axios'
 {
 
