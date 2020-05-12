2c355b69e22981253e5d575f895432e5bb411e75
bluelovers <codelovers@users.sourceforge.net>
2020-05-13 03:59:34 +0800

    feat: auto task support phpwind

--- script/task/dz/index.ts
+++ script/task/dz/index.ts
@@ -3,6 +3,7 @@
  */
 
 import ApiClient from 'discuz-api/lib/index';
+import PHPWindClient from 'phpwind-api/lib/index';
 import Bluebird from 'bluebird';
 import { config } from 'dotenv'
 import { consoleDebug } from '@node-novel/cached-masiro/script/util';
@@ -15,13 +16,15 @@ export interface IMY_HASHED_JSON_ROW
 {
 	baseURL: string,
 	cookies: Record<string, string>,
+
+	siteType?: string | 'discuz' | 'phpwind',
 }
 
 export default lazyRun(async () => {
 
 	await Bluebird.resolve().tap(e => config()).catch(e => null);
 
-	consoleDebug.info(`執行 discuz 模組`);
+	//consoleDebug.info(`執行 discuz 模組`);
 
 	await Bluebird.resolve(process.env.MY_HASHED_JSON)
 		.then<IMY_HASHED_JSON_ROW[]>((envValue) => {
@@ -29,16 +32,17 @@ export default lazyRun(async () => {
 		})
 		.catch(e => [] as IMY_HASHED_JSON_ROW[])
 		.tap(ls => consoleDebug.info(`list count:`, ls.length))
-		.each(async (options) => {
+		.each(async (options: IMY_HASHED_JSON_ROW) => {
 
-			let { baseURL, cookies } = options;
+			let { baseURL, cookies, siteType } = options;
 
-			console.dir(baseURL);
+			consoleDebug.info(siteType, baseURL);
 
-			const api = new ApiClient({
+			const api = new (siteType === 'phpwind' ? PHPWindClient : ApiClient)({
 				baseURL,
 			});
 
+			// @ts-ignore
 			api.loginByCookiesSync(cookies);
 
 			await api.doAutoTaskList(((eventName, data) => {
@@ -92,7 +96,8 @@ export default lazyRun(async () => {
 				.catch(e => null)
 			;
 
-			await api.noticeView('app');
+			// @ts-ignore
+			await api.noticeView?.('app');
 
 		})
 	;
