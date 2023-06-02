0f423b73866211aafbaf0c1415b27b05992b6875
bluelovers <codelovers@users.sourceforge.net>
2023-06-03 04:30:03 +0800

    .

--- script/ci/test/try-cache.js
+++ script/ci/test/try-cache.js
@@ -1,12 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
 const index_1 = require("@node-novel/site-cache-util/lib/index");
 const project_root_1 = require("../../project-root");
-const fs_extra_1 = tslib_1.__importDefault(require("fs-extra"));
+const fs_extra_1 = require("fs-extra");
 exports.default = (0, index_1.lazyRun)(async () => {
     let __root = project_root_1.__rootWs;
-    await fs_extra_1.default.ensureFile(index_1.path.join(__root, 'packages/@node-novel', 'cached-dmzj', 'test/temp', 'cache.ensure.txt'));
+    await (0, fs_extra_1.ensureFile)(index_1.path.join(__root, 'packages/@node-novel', 'cached-dmzj', 'test/temp', 'cache.ensure.txt'));
 }, {
     pkgLabel: __filename,
 });
--- script/ci/test/try-cache.ts
+++ script/ci/test/try-cache.ts
@@ -2,13 +2,13 @@ import { assert, expect } from 'chai';
 import FastGlob from '@bluelovers/fast-glob/bluebird';
 import { lazyRun, path, console } from '@node-novel/site-cache-util/lib/index';
 import { __rootWs } from '../../project-root';
-import fs from 'fs-extra';
+import { ensureFile } from 'fs-extra';
 
 export default lazyRun(async () => {
 
 	let __root = __rootWs;
 
-	await fs.ensureFile(path.join(__root, 'packages/@node-novel', 'cached-dmzj', 'test/temp', 'cache.ensure.txt'));
+	await ensureFile(path.join(__root, 'packages/@node-novel', 'cached-dmzj', 'test/temp', 'cache.ensure.txt'));
 
 }, {
 	pkgLabel: __filename,
